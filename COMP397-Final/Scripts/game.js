d264a2702600958ca2f1aa599b5efb74e14b0a31
SirLouisSmith <someonewholives@hotmail.com>
2015-03-30 11:33:05 -0400

    Created Initial VS Project

---
+++ COMP397-Final/Scripts/game.js
@@ -0,0 +1,21 @@
+/// <reference path="typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+//Author: Louis Smith
+//File: game.ts
+//Last modified: 30/03/2015
+//Description:
+var canvas;
+var stage;
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); // Enable mouse events
+    createjs.Ticker.setFPS(60); // 60 frames per second
+    createjs.Ticker.addEventListener("tick", gameLoop);
+}
+function gameLoop() {
+}
+//# sourceMappingURL=game.js.map
---
+++ COMP397-Final/Scripts/game.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+
+//Author: Louis Smith
+//File: game.ts
+//Last modified: 30/03/2015
+//Description:
+
+var canvas;
+var stage: createjs.Stage;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); // Enable mouse events
+    createjs.Ticker.setFPS(60); // 60 frames per second
+    createjs.Ticker.addEventListener("tick", gameLoop);
+} 
+
+function gameLoop() {
+
+}
